d70ce2977ec5c254669f415aef3e1fac79977d1a
Paulo de Assis <paulo.henrique.hunter@gmail.com>
2023-10-09 15:49:24 -0300

    feat: Add the .next js folder

---
+++ .next/static/chunks/app/[userUrl]/car_dealerships/page-f7dfdd15b64c826f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[340,555,226,943,301,866,250,667,57,534,457,186,53,38,275],{4320:function(n,e,u){Promise.resolve().then(u.t.bind(u,6964,23))}},function(n){n.O(0,[964,971,864,744],function(){return n(n.s=4320)}),_N_E=n.O()}]);
---
+++ .next/static/chunks/app/[userUrl]/lead_generation/page-ee4bd0e5d0751b4d.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[186,555,340,226,943,301,866,250,667,57,534,457,53,38,275],{4320:function(n,e,u){Promise.resolve().then(u.t.bind(u,6964,23))}},function(n){n.O(0,[964,971,864,744],function(){return n(n.s=4320)}),_N_E=n.O()}]);
